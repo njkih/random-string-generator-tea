12d18e7bb7729314bcca57b126f45ed7cdf4e62a
dfghjkcvbnmnbvcx <163094726+njkih@users.noreply.github.com>
2024-03-17 00:28:03 +0700

    init project

---
+++ index.js
@@ -0,0 +1,16 @@
+// index.js
+
+function generateRandomString(length) {
+    const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    let randomString = '';
+    for (let i = 0; i < length; i++) {
+      const randomIndex = Math.floor(Math.random() * characters.length);
+      randomString += characters[randomIndex];
+    }
+    return randomString;
+  }
+  
+  const length = 10;
+  const randomString = generateRandomString(length);
+  console.log(`Random string of length ${length}:`, randomString);
+  
